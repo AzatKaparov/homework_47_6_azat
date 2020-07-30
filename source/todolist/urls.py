9e9ffb658b56f8486d2107c6ed57c3c8a888b17e
Azat Kaparov <azatkaparov843@gmail.com>
2020-07-30 19:51:36 +0600

    Доработка функции update Добавление функции удаления задач

--- source/todolist/urls.py
+++ source/todolist/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, task_add_view, task_view, task_update_view
+from webapp.views import index_view, task_add_view, task_view, task_update_view, delete_view
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', index_view, name='index'),
     path('tasks/add/', task_add_view, name='task_add'),
     path('task/<int:pk>/', task_view, name='task_view'),
-    path('task/<int:pk>/update', task_update_view, name='task_update')
+    path('task/<int:pk>/update', task_update_view, name='task_update'),
+    path('task/<int:pk>/delete', delete_view, name='task_delete')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Task, STATUS_CHOICES
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponseNotAllowed
 from django.urls import reverse
 
 
@@ -44,15 +44,40 @@ def task_view(request, pk):
 def task_update_view(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method =='GET':
-        return render(request, 'update.hmtl', context={
+        return render(request, 'update.html', context={
             'status_choices': STATUS_CHOICES,
             'task': task,
         })
     elif request.method =='POST':
+        errors = {}
         task.description = request.POST.get('description')
+        if not task.description:
+            errors['description'] = "Это поле обязательно!"
         task.status = request.POST.get('status')
+        if not task.status:
+            errors['status'] = "Это поле обязательно!"
         task.date = request.POST.get('date')
+        if not task.date:
+            errors['date'] = "Это поле обязательно!"
         task.more = request.POST.get('more')
+
+        if errors:
+            return render(request, 'update.html', context={
+                'status_choices': STATUS_CHOICES,
+                'task': task,
+                'errors': errors,
+            })
         task.save()
 
         return redirect('task_view', pk=task.pk)
+
+
+def delete_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'delete.html', context={'task': task})
+    elif request.method == 'POST':
+        task.delete()
+        return redirect('index')
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
